3803696ece4d6a7e4ed34974740634a6491327f3
Anton T. Adamsen <antontandrup@gmail.com>
2026-05-27 09:48:06 +0200

    Add globe key

--- keyboards/splitkb/halcyon/kyria/keymaps/anton_hlc/rules.mk
+++ keyboards/splitkb/halcyon/kyria/keymaps/anton_hlc/rules.mk
@@ -2,6 +2,9 @@ ENCODER_MAP_ENABLE   = no
 TAP_DANCE_ENABLE     = yes
 CAPS_WORD_ENABLE     = yes
 OS_DETECTION_ENABLE  = yes
+# Needed so the Apple Globe key (Consumer page) can be held as a modifier
+# for chords like Globe+E, Globe+F, etc.
+KEYBOARD_SHARED_EP   = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
